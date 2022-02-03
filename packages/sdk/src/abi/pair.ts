f83be154b6ddca20c7d3a939a98a6af51731a7d7
Emmanuel Antony <emmanuelantony2000@gmail.com>
2022-02-03 15:58:59 +0530

    Fixed the issue with the abi

--- packages/sdk/src/abi/pair.ts
+++ packages/sdk/src/abi/pair.ts
@@ -5,8 +5,8 @@ export default [
   'function totalReserves(uint256) view returns ((uint128,uint128))',
   'function totalLiquidity(uint256) view returns (uint256)',
   'function liquidityOf(uint256,address) view returns (uint256)',
-  'function totalClaims(uint256) view returns ((uint128,uint128))',
-  'function claimsOf(uint256,address) view returns ((uint128,uint128)) ',
+  'function totalClaims(uint256) view returns ((uint112,uint112,uint112,uint112))',
+  'function claimsOf(uint256,address) view returns ((uint112,uint112,uint112,uint112)) ',
   'function totalDebtCreated(uint256) view returns (uint120)',
   'function duesOf(uint256,address) view returns ((uint112,uint112,uint32)[])',
 ];
