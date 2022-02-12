5e1c5316a22f57f7940e1bdcdff2ef761d192c90
Emmanuel Antony <emmanuelantony2000@gmail.com>
2022-02-12 13:56:11 +0530

    Added Factory addressses to constants

--- packages/sdk/src/constants/address.ts
+++ packages/sdk/src/constants/address.ts
@@ -2,3 +2,8 @@ export const CONVENIENCE: { [chainID: number]: string } = {
   4: '0x6bb008dc06B0Db1b3efCe0F7cB5901816224Da5d',
   80001: '0xe2778215b4Dfbc3fd24bc072AE42D55bee981Fe2',
 };
+
+export const FACTORY: { [chainID: number]: string } = {
+  4: '0x32514b9Be3393DE126113ce5880d77335cfFb264',
+  80001: '0x623eA65563c9c3ec6B61e567B4034A216ACE5d0E',
+};
