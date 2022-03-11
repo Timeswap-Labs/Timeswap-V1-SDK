56ec9d817eefdfed11d2117c4b54272855a172ee
Emmanuel Antony <emmanuelantony2000@gmail.com>
2022-03-11 12:30:10 +0530

    Updated the addresses for rinkeby

--- packages/sdk/src/constants/address.ts
+++ packages/sdk/src/constants/address.ts
@@ -1,9 +1,9 @@
 export const CONVENIENCE: { [chainID: number]: string } = {
-  4: '0x6bb008dc06B0Db1b3efCe0F7cB5901816224Da5d',
+  4: '0xefE81D4Aed8E58706b1B592d372DA21c46e64bBc',
   80001: '0x5d33c9c1b4b14a727dd8dcd14b99d87edcc7777f',
 };
 
 export const FACTORY: { [chainID: number]: string } = {
-  4: '0x32514b9Be3393DE126113ce5880d77335cfFb264',
+  4: '0x87838724523EB63fb3Af8418b3f14f00d4AcED08',
   80001: '0x0a2934eA5891661c2E9052Ab57f78e6C931b2339',
 };
