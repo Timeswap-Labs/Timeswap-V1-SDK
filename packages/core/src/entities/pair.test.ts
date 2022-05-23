d41c21db6c5a94e18fedc944c3c96a765c83a275
Emmanuel Antony <emmanuelantony2000@gmail.com>
2022-05-23 08:52:57 +0530

    Added tests for add min

--- packages/core/src/entities/pair.test.ts
+++ packages/core/src/entities/pair.test.ts
@@ -1,4 +1,4 @@
-import { Uint112, Uint256 } from '../uint';
+import { Uint112, Uint16, Uint256, Uint40 } from '../uint';
 import { Pair } from './pair';
 
 describe('Pair tests', () => {
@@ -29,6 +29,70 @@ describe('Pair tests', () => {
       new Uint112(5000000000000000000n),
       new Uint256(1649835506n),
       new Uint256(29156019n)
+    );
+  });
+
+  it('Test 3 (Add min lend)', () => {
+    Pair.calculateLendGivenPercent(
+      {
+        x: new Uint112(42007650537n),
+        y: new Uint112(439305738839n),
+        z: new Uint112(32905668061664118996234n),
+      },
+      new Uint256(1650366000n),
+      new Uint112(10000000n),
+      new Uint40(0n),
+      new Uint256(1649835506n),
+      new Uint16(434),
+      new Uint16(86)
+    );
+  });
+
+  it('Test 4 (Add min lend)', () => {
+    Pair.calculateLendGivenPercent(
+      {
+        x: new Uint112(42007650537n),
+        y: new Uint112(439305738839n),
+        z: new Uint112(32905668061664118996234n),
+      },
+      new Uint256(1650366000n),
+      new Uint112(10000000n),
+      new Uint40(2n ** 32n),
+      new Uint256(1649835506n),
+      new Uint16(434),
+      new Uint16(86)
+    );
+  });
+
+  it('Test 5 (Add min borrow)', () => {
+    Pair.calculateBorrowGivenPercent(
+      {
+        x: new Uint112(42007650537n),
+        y: new Uint112(439305738839n),
+        z: new Uint112(32905668061664118996234n),
+      },
+      new Uint256(1650366000n),
+      new Uint112(10000000n),
+      new Uint40(0n),
+      new Uint256(1649835506n),
+      new Uint16(434),
+      new Uint16(86)
+    );
+  });
+
+  it('Test 6 (Add min borrow)', () => {
+    Pair.calculateBorrowGivenPercent(
+      {
+        x: new Uint112(42007650537n),
+        y: new Uint112(439305738839n),
+        z: new Uint112(32905668061664118996234n),
+      },
+      new Uint256(1650366000n),
+      new Uint112(10000000n),
+      new Uint40(2n ** 32n),
+      new Uint256(1649835506n),
+      new Uint16(434),
+      new Uint16(86)
     );
   });
 });
